99ec14b4a39518c2e3e413ed01e7acd979a06b56
KhangPham05 <khangpham05tv@gmail.com>
2026-09-22 18:24:56 -0700

    Set up Spring Boot project with db config

---
+++ src/main/java/com/tutorslot/TutorSlotApplication.java
@@ -0,0 +1,12 @@
+package com.tutorslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSlotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorSlotApplication.class, args);
+    }
+}
